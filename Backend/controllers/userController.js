89be9144fa2b4c7f4f5ff4a969faee455c257bfd
Aryan0778 <your-current-github-email@example.com>
2026-05-22 15:01:13 +0530

    Admin function + Controller fix

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -78,6 +78,7 @@ const adminLogin = async(req,res) =>{
         
     try {
         
+        
         const{email,password} = req.body
         if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
             const token = jwt.sign(email+password,process.env.JWT_SECRET);
